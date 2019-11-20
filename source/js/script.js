ceccfb868a70af402aa7c721d8a8914cd1164971
Miuniverso <m.yana93@mail.ru>
2019-11-20 19:02:25 +0300

    Верстка всех страниц

---
+++ source/js/script.js
@@ -0,0 +1,26 @@
+/ Кастомный маркер мороженки на карте
+
+ymaps.ready(function () {
+  var myMap = new ymaps.Map('map', {
+          center: [59.938631, 30.323055],
+          zoom: 18
+      }, {
+          searchControlProvider: 'yandex#search'
+      }),
+
+      MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+          '<div style="color: #FFFFFF; font-weight: bold;"">$[properties.iconContent]</div>'
+      ),
+
+      myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+          hintContent: 'Cat Energy',
+      }, {
+          iconLayout: 'default#image',
+          iconImageHref: '../img/map-pin.png',
+          iconImageSize: [124, 106],
+          iconImageOffset: [-40, -140]
+      });
+
+  myMap.geoObjects
+      .add(myPlacemark);
+});
