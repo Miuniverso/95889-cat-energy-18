b0bca5e7ff704288e266eeafb824373e4840ccfd
Miuniverso <m.yana93@mail.ru>
2019-11-26 19:16:20 +0300

    Починила карту

--- source/js/script.js
+++ source/js/script.js
@@ -1,4 +1,4 @@
-/ Кастомный маркер мороженки на карте
+// Кастомный маркер мороженки на карте
 
 ymaps.ready(function () {
   var myMap = new ymaps.Map('map', {
@@ -16,7 +16,7 @@ ymaps.ready(function () {
           hintContent: 'Cat Energy',
       }, {
           iconLayout: 'default#image',
-          iconImageHref: '../img/map-pin.png',
+          iconImageHref: 'img/map-pin.png',
           iconImageSize: [124, 106],
           iconImageOffset: [-40, -140]
       });
